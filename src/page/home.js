772d5a90ba5a2344cff05bafe7ec9dc5bce64654
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-04-15 00:34:01 +0700

    'feature todo'

--- src/page/home.js
+++ src/page/home.js
@@ -1,12 +1,24 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import SoalSatu from "../components/soal-1";
+import React, { useState } from "react";
+
+import SoalSatu from "../components/soal-1/soal-1";
+
+import TodoApp from "../components/todo-2-ed/TodoApp";
 
 function Home() {
   return (
-    <Container>
+    <>
+      {
+        /* <Container>
       <SoalSatu />
-    </Container>
+    </Container> */
+
+        // <div className="todo-app">
+        //   <TodoList />
+        // </div>
+        <TodoApp />
+        // <TesUI />
+      }
+    </>
   );
 }
 
